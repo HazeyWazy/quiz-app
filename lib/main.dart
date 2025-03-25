f1c5dfbd3b93239c145676f8f0600ffb9bea22b3
Htet Yamin Ei 47743220 <htetyamin.ei@students.mq.edu.au>
2025-03-25 21:26:40 +1100

    Update start screen with image, text and button

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,20 @@ import 'package:quiz_app/start_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 13, 73, 151),
+                  Color.fromARGB(255, 15, 79, 168),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: const StartScreen()),
       ),
     ),
   );
