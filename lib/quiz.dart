0fe7a48710b015aa84ace5f4ac385b78dca6413e
Htet Yamin Ei 47743220 <htetyamin.ei@students.mq.edu.au>
2025-03-26 20:31:26 +1100

    Pass data to result screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -53,7 +53,9 @@ class _QuizState extends State<Quiz> {
     }
 
     if (activeScreen == 'results-screen') {
-      screenWidget = const ResultsScreen();
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+      );
     }
 
     // final screenWidget = activeScreen == 'start-screen'
